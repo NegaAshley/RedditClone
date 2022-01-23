99f97040b88019b651284fac1dd7c713df8a8976
Ashley Xu <amx8198@ads.northwestern.edu>
2022-01-23 13:43:37 -0600

    Adding partials and BootStrap sytling.  Updating gif for readme.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const redditData = require('./data.json');
 
+app.use(express.static(path.join(__dirname, '/public')));//Serve static assets, such as CSS.  Takes absolute path to index.js file and adding on public.
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
